440131202fb8845923949b7caefdaba6c5425b60
MandemGibson <themaingib@gmail.com>
2024-12-14 23:57:19 +0000

    pure, view functions

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.7; //Declare version
 
 contract SimpleStorage {
-    
+    //boolean, string, uint, int, address, bytes
+    uint256 public favoriteNumber;//This gets initialized to zero
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
 }
+//0xd9145CCE52D386f254917e481eB44e9943F39138
